46a0285b9efc2e5532b3c6d492c596026bdbb5ba
Marcin Maslanka <maslankam92@gmail.com>
2015-11-08 12:27:24 +0100

    4 Added markup template, paralax hero image

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 /**
  * Created by MarcinM on 06-Nov-15.
  */
+$(function(){
+
+    /** main module function
+     * which contain all functions on the page.
+     * Make functions more visible and organized **/
+
+    var Application = function(){
+
+        function init(){
+            console.log("napisz, ze dziala");
+        }
+
+        function fullPage(){
+            $('#fullpage').fullpage({
+                anchors: ['firstPage', 'secondPage', '3rdPage'],
+                sectionsColor: ['#fff', '#fff', '#F8F2E2', '#3BC391', '#EB4549', '#2F79C3', '#374047'],
+                responsiveWidth: 900,
+                slidesNavigation: true
+            });
+        }
+
+        return {
+            init: init,
+            fullPage: fullPage
+        }
+
+    };
+
+    var app = new Application();
+    app.init();
+    app.fullPage();
+
+
+});
