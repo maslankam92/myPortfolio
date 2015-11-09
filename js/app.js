fc60a67c426afaedcf43738cecb0c418bf4e6251
Marcin Maslanka <maslankam92@gmail.com>
2015-11-09 08:08:41 +0100

    5 starting over, plans changed

--- js/app.js
+++ js/app.js
@@ -15,16 +15,30 @@ $(function(){
 
         function fullPage(){
             $('#fullpage').fullpage({
-                anchors: ['firstPage', 'secondPage', '3rdPage'],
-                sectionsColor: ['#fff', '#fff', '#F8F2E2', '#3BC391', '#EB4549', '#2F79C3', '#374047'],
+                sectionsColor: ['#F8F2E2', '#3BC391', '#EB4549', '#2F79C3', '#374047'],
                 responsiveWidth: 900,
-                slidesNavigation: true
+                slidesNavigation: true,
+                navigation: true,
+                anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+                menu: '#menu'
             });
         }
 
+        //function scrollPage(){
+        //    var links = $(".main-nav-btn a");
+        //
+        //    links.on("click", function(){
+        //        $("html, body").animate({                               //if links are clicked, animate (1s) html and body
+        //            scrollTop: $($(this).attr("href")).offset().top     //to the top of the clicked anchor's href
+        //        }, 700, 'swing');
+        //        console.log("smiga");
+        //    });
+        //}
+
         return {
             init: init,
-            fullPage: fullPage
+            fullPage: fullPage,
+            scrollPage: scrollPage
         }
 
     };
@@ -32,6 +46,7 @@ $(function(){
     var app = new Application();
     app.init();
     app.fullPage();
+    app.scrollPage();
 
 
 });
