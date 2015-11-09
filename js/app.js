51ccf6ca4dc5bc51d5fa086e67d5cbf14a6f2a76
Marcin Maslanka <maslankam92@gmail.com>
2015-11-09 19:42:20 +0100

    7 added header with navigation and about section

--- js/app.js
+++ js/app.js
@@ -13,21 +13,27 @@ $(function(){
             console.log("napisz, ze dziala");
         }
 
-        function parallax(){
-            var scene = document.getElementById('scene');
-            var parallax = new Parallax(scene);
+        function scrollPage(){
+            var links = $("a");
+
+            links.click(function(){
+                $("html, body").animate({                               //if links are clicked, animate (1s) html and body
+                    scrollTop: $($(this).attr("href")).offset().top     //to the top of the clicked anchor's href
+                }, 700, 'swing');
+                return false;
+            });
         }
 
         return {
             init: init,
-            parallax: parallax
+            scrollPage: scrollPage
         }
 
     };
 
     var app = new Application();
     app.init();
-    app.parallax();
+    app.scrollPage();
 
 
 });
